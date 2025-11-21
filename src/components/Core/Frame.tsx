9d2057f0e9f5e89ae8ba7f175cf19e058f56b761
andr1ww <tiva@itztiva.com>
2025-11-21 16:56:39 -0500

    frame & particlesssss

---
+++ src/components/Core/Frame.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Window } from "@tauri-apps/api/window";
+import { HiX, HiMinus } from "react-icons/hi";
+
+const Frame: React.FC = () => {
+  return (
+    <div
+      data-tauri-drag-region
+      className="fixed w-screen h-6 top-0 left-0 flex justify-end items-center z-50"
+    >
+      <div className="flex items-center ml-3 mt-1"></div>
+      <div className="flex text-white h-full">
+        <button
+          onClick={() => Window.getCurrent().minimize()}
+          className="cursor-pointer  w-10 h-7 flex justify-center items-center hover:bg-white/20 transition-all rounded-sm hover:backdrop-blur-2xl duration-220"
+        >
+          <HiMinus />
+        </button>
+
+        <button
+          onClick={() => Window.getCurrent().close()}
+          className="cursor-pointer w-10 h-7 flex justify-center items-center hover:bg-red-500/50 transition-all rounded-sm hover:backdrop-blur-2xl duration-220"
+        >
+          <HiX />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Frame;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { HashRouter, Route, Routes, useLocation } from "react-router-dom";
+import Particles from "./components/Core/Particles";
+import Frame from "./components/Core/Frame";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const App = () => {
+  return (
+    <>
+      <Frame />
+      <Particles quantity={70} />
+
+      <Routes></Routes>
+    </>
+  );
+};
+
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+root.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
